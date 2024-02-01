a06c8a96926e6764b6762eb66cef0671654542ad
Aldo <aldo.medina.z@gmail.com>
2024-02-01 16:26:12 -0300

    image loader test

--- app/[lng]/projects/page.jsx
+++ app/[lng]/projects/page.jsx
@@ -7,6 +7,9 @@ import Image from 'next/image'
 import { Suspense, useState } from 'react'
 import Marquee from 'react-fast-marquee'
 import { featuredProjects, otherProjects } from './content'
+import { Trans } from 'react-i18next'
+import { useTranslation } from '@/i18n/client'
+
 const CardsScene = dynamic(() => import('@/components/canvas/CardsScene').then((mod) => mod.CardsScene), { ssr: false })
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
@@ -14,15 +17,15 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
 })
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
-export default function Page() {
+export default function Page({ params: { lng } }) {
+  const { t } = useTranslation(lng, 'projects')
   const [active, setActive] = useState(false)
   return (
     <>
       <div className='flex  flex-col'>
         <div className='mx-auto flex h-96 w-full items-end p-4 text-sm lg:p-8 2xl:w-4/5 2xl:text-lg'>
           <p className='max-w-52 leading-5 2xl:max-w-72'>
-            <strong>Projects. </strong>This section presents projects showcasing work as a front-end developer, creative
-            developer, and designer, highlighting diverse skills in web-based solutions.
+            <Trans i18nKey={t('intro')} />
           </p>
         </div>
         <div className=' flex w-full flex-col border-b border-gray-600 font-mono leading-none'>
@@ -36,7 +39,14 @@ export default function Page() {
               <Marquee autoFill className='z-0 overflow-hidden' direction={i % 2 === 0 ? 'right' : 'left'} speed={20}>
                 <span className='mr-4 select-none text-9xl uppercase	'>{el.title}</span>
                 <div className='mr-4 mt-4 rounded border-2 border-gray-950'>
-                  <Image src={el.imageUrl} alt='teapot' width={100 * el.ratio} height={100} placeholder='blur' />
+                  <Image
+                    src={el.imageUrl}
+                    alt='teapot'
+                    width={100 * el.ratio}
+                    height={100}
+                    placeholder='blur'
+                    blurDataURL={el.blurDataUrl}
+                  />
                 </div>
               </Marquee>
             </div>
@@ -45,17 +55,24 @@ export default function Page() {
       </div>
       <div className='mx-auto flex min-h-48 w-full items-end p-4 text-sm leading-5 lg:p-8 2xl:w-4/5 2xl:text-lg'>
         <p className='flex-1 sm:max-w-52 2xl:max-w-72'>
-          The following section exposes further work experiences as a front-end developer and designer, focusing on a
-          variety of web-based projects.
+          <Trans i18nKey={t('intro-2')} />
         </p>
       </div>
       <div className='bg-gray-950'>
         <div className='mx-auto hidden w-full justify-between p-4 font-mono text-4xl uppercase text-gray-100 lg:flex lg:px-8 2xl:w-4/5 2xl:text-lg'>
           {/* <div className='w-24'>[1.{i < 5 ? '0' + (5 + i) : '' + (5 + i)}]</div> */}
-          <div className='basis-1/4'>project name</div>
-          <div className='basis-1/4'>working for</div>
-          <div className='basis-1/4'>role</div>
-          <div className='w-24'>year</div>
+          <div className='basis-1/4'>
+            <Trans i18nKey={t('header-name')} />
+          </div>
+          <div className='basis-1/4'>
+            <Trans i18nKey={t('header-working-for')} />
+          </div>
+          <div className='basis-1/4'>
+            <Trans i18nKey={t('header-role')} />
+          </div>
+          <div className='w-24'>
+            <Trans i18nKey={t('header-year')} />
+          </div>
         </div>
       </div>
       {otherProjects.map((el, i) => (
@@ -64,9 +81,13 @@ export default function Page() {
             <div className='font-bold lg:basis-1/4'>
               {el.name} <span className='text-xl font-medium lg:hidden'>— {el.year}</span>
             </div>
-            <span className='mt-4 font-sans text-sm normal-case lg:hidden'>working for:</span>
+            <span className='mt-4 font-sans text-sm normal-case lg:hidden'>
+              <Trans i18nKey={t('mobile-working-for')} />
+            </span>
             <div className='lg:basis-1/4'>{el.workingFor}</div>
-            <span className='mt-4 font-sans text-sm normal-case lg:hidden'>as:</span>
+            <span className='mt-4 font-sans text-sm normal-case lg:hidden'>
+              <Trans i18nKey={t('mobile-as')} />
+            </span>
             <div className='lg:basis-1/4'>{el.roles}</div>
             <div className='hidden w-24 lg:block'>{el.year}</div>
           </div>
